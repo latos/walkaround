latos/walkaround

commit cf9459a4c8f4d2293f4ad42a510e8c16872bd002

ObjectStoreSelector: Move to slob layer (and rename to SlobStoreSelector).
Together with the previous change, this will allow the slob layer to
use the task queue to maintain eventually consistent caches.

src/com/google/walkaround/wave/server/admin/StoreViewHandler.java
@@ -21,6 +21,7 @@ import com.google.gxp.base.GxpContext;
 import com.google.gxp.html.HtmlClosure;
 import com.google.inject.Inject;
 import com.google.walkaround.slob.server.MutationLog;
+import com.google.walkaround.slob.server.SlobStoreSelector;
 import com.google.walkaround.slob.server.MutationLog.DeltaIterator;
 import com.google.walkaround.slob.shared.SlobId;
 import com.google.walkaround.util.server.RetryHelper.PermanentFailure;
@@ -31,7 +32,6 @@ import com.google.walkaround.util.server.servlet.AbstractHandler;
 import com.google.walkaround.util.server.servlet.BadRequestException;
 import com.google.walkaround.wave.server.Flag;
 import com.google.walkaround.wave.server.FlagName;
-import com.google.walkaround.wave.server.ObjectStoreSelector;
 import com.google.walkaround.wave.server.StoreType;
 import com.google.walkaround.wave.server.gxp.Admin;
 import com.google.walkaround.wave.server.gxp.StoreViewFragment;
@@ -56,7 +56,7 @@ public class StoreViewHandler extends AbstractHandler {
   private static final Logger log = Logger.getLogger(StoreViewHandler.class.getName());
 
   @Inject CheckedDatastore datastore;
-  @Inject ObjectStoreSelector storeSelector;
+  @Inject SlobStoreSelector storeSelector;
   @Inject @Flag(FlagName.ANALYTICS_ACCOUNT) String analyticsAccount;
 
   @Override
@@ -76,7 +76,7 @@ public class StoreViewHandler extends AbstractHandler {
         SlobId objectId = new SlobId(id);
         CheckedTransaction tx = datastore.beginTransaction();
         try {
-          MutationLog mutationLog = storeSelector.getMutationLogFactory(StoreType.parse(storeType))
+          MutationLog mutationLog = storeSelector.get(storeType).getMutationLogFactory()
               .create(tx, objectId);
           objectVersion = mutationLog.getVersion();
           if (!historyStart.isEmpty()) {

src/com/google/walkaround/slob/server/AffinityMutationProcessor.java
@@ -24,7 +24,6 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import com.google.appengine.api.backends.BackendService;
 import com.google.appengine.api.memcache.Expiration;
-import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheService.SetPolicy;
 import com.google.appengine.api.urlfetch.FetchOptions;
 import com.google.appengine.api.urlfetch.HTTPHeader;

src/com/google/walkaround/slob/server/MutationLog.java
@@ -127,16 +127,21 @@ public class MutationLog {
     }
   }
 
-  public Key makeRootEntityKey(SlobId objectId) {
+  static Key makeRootEntityKey(String entityGroupKind, SlobId objectId) {
     Key key = KeyFactory.createKey(entityGroupKind, objectId.getId());
-    Assert.check(parseRootEntityKey(key).equals(objectId), "Mismatch: %s, %s", objectId, key);
+    Assert.check(parseRootEntityKey(entityGroupKind, key).equals(objectId),
+        "Mismatch: %s, %s", objectId, key);
     return key;
   }
 
-  public SlobId parseRootEntityKey(Key key) {
+  static SlobId parseRootEntityKey(String entityGroupKind, Key key) {
     Preconditions.checkArgument(entityGroupKind.equals(key.getKind()),
         "Key doesn't have kind %s: %s", entityGroupKind, key);
     return new SlobId(key.getName());
+  }
+  
+  private Key makeRootEntityKey(SlobId slobId) {
+    return makeRootEntityKey(entityGroupKind, slobId);
   }
 
   private Key makeDeltaKey(SlobId objectId, long version) {

src/com/google/walkaround/slob/server/SlobFacilities.java
@@ -1,0 +1,25 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+
+package com.google.walkaround.slob.server;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.walkaround.slob.server.MutationLog.MutationLogFactory;
+import com.google.walkaround.slob.shared.SlobId;
+
+/**
+ * Offers access to various classes that operate on a slob type.
+ *
+ * @author ohler@google.com (Christian Ohler)
+ */
+public interface SlobFacilities {
+
+  SlobStore getSlobStore();
+  MutationLogFactory getMutationLogFactory();
+  LocalMutationProcessor getLocalMutationProcessor();
+  
+  String getRootEntityKind();
+
+  Key makeRootEntityKey(SlobId slobId);
+  SlobId parseRootEntityKey(Key key);
+
+}

src/com/google/walkaround/slob/server/SlobMessageRouter.java
@@ -26,7 +26,6 @@ import com.google.appengine.api.channel.ChannelFailureException;
 import com.google.appengine.api.channel.ChannelMessage;
 import com.google.appengine.api.channel.ChannelService;
 import com.google.appengine.api.memcache.Expiration;
-import com.google.appengine.api.memcache.MemcacheService;
 import com.google.appengine.api.memcache.MemcacheService.SetPolicy;
 import com.google.common.base.Objects;
 import com.google.common.collect.Sets;

src/com/google/walkaround/slob/server/SlobStoreImpl.java
@@ -64,6 +64,7 @@ public class SlobStoreImpl implements SlobStore {
   private final AccessChecker accessChecker;
   private final PostMutateHook postMutateHook;
   private final PreCommitHook preCommitHook;
+  private final String rootEntityKind;
 
   @Inject
   public SlobStoreImpl(CheckedDatastore datastore,
@@ -74,7 +75,8 @@ public class SlobStoreImpl implements SlobStore {
       LocalMutationProcessor localProcessor,
       AccessChecker accessChecker,
       PostMutateHook postMutateHook,
-      PreCommitHook preCommitHook) {
+      PreCommitHook preCommitHook,
+      @SlobRootEntityKind String rootEntityKind) {
     this.datastore = datastore;
     this.mutationLogFactory = mutationLogFactory;
     this.messageRouter = messageRouter;
@@ -82,6 +84,7 @@ public class SlobStoreImpl implements SlobStore {
     this.accessChecker = accessChecker;
     this.postMutateHook = postMutateHook;
     this.preCommitHook = preCommitHook;
+    this.rootEntityKind = rootEntityKind;
   }
 
   @Override
@@ -115,7 +118,8 @@ public class SlobStoreImpl implements SlobStore {
           snapshot = null;
         }
         if (version == 0) {
-          throw new SlobNotFoundException("Object " + objectId + " not found");
+          throw new SlobNotFoundException(
+              "Object " + objectId + " (" + rootEntityKind + ") not found");
         }
       } finally {
         tx.rollback();

src/com/google/walkaround/slob/server/SlobStoreSelector.java
@@ -1,0 +1,47 @@
+/*
+ * Copyright 2011 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.walkaround.slob.server;
+
+import com.google.inject.Inject;
+import com.google.inject.Provider;
+import com.google.walkaround.util.server.servlet.BadRequestException;
+
+import java.util.Map;
+
+/**
+ * Maps "store type" strings (= root entity kind) to the corresponding SlobFacilities.
+ *
+ * @author ohler@google.com (Christian Ohler)
+ */
+public class SlobStoreSelector {
+
+  private final Map<String, Provider<SlobFacilities>> facilities;
+
+  @Inject
+  public SlobStoreSelector(Map<String, Provider<SlobFacilities>> facilities) {
+    this.facilities = facilities;
+  }
+
+  public SlobFacilities get(String storeType) {
+    Provider<SlobFacilities> provider = facilities.get(storeType);
+    if (provider != null) {
+      return provider.get();
+    }
+    throw new BadRequestException("Unknown store type: " + storeType);
+  }
+
+}

src/com/google/walkaround/slob/server/StoreModuleHelper.java
@@ -16,11 +16,16 @@
 
 package com.google.walkaround.slob.server;
 
+import com.google.appengine.api.datastore.Key;
+import com.google.inject.Inject;
 import com.google.inject.Module;
 import com.google.inject.PrivateBinder;
 import com.google.inject.PrivateModule;
+import com.google.inject.Provider;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
+import com.google.inject.multibindings.MapBinder;
 import com.google.walkaround.slob.server.MutationLog.MutationLogFactory;
+import com.google.walkaround.slob.shared.SlobId;
 
 import java.lang.annotation.Annotation;
 import java.util.logging.Logger;
@@ -42,18 +47,58 @@ public class StoreModuleHelper {
         .build(factoryClass);
   }
 
+  private static class FacilitiesImpl implements SlobFacilities {
+    // These are providers since many users will only need one.
+    @Inject Provider<SlobStore> slobStore;
+    @Inject Provider<LocalMutationProcessor> localMutationProcessor;
+    @Inject Provider<MutationLogFactory> mutationLogFactory;
+    @Inject @SlobRootEntityKind String rootEntityKind;
+
+    @Override public String toString() {
+      return "SlobFacilities(" + rootEntityKind + ")";
+    }
+
+    @Override public SlobStore getSlobStore() {
+      return slobStore.get();
+    }
+
+    @Override public LocalMutationProcessor getLocalMutationProcessor() {
+      return localMutationProcessor.get();
+    }
+
+    @Override public MutationLogFactory getMutationLogFactory() {
+      return mutationLogFactory.get();
+    }
+
+    @Override public String getRootEntityKind() {
+      return rootEntityKind;
+    }
+
+    @Override public Key makeRootEntityKey(SlobId slobId) {
+      return MutationLog.makeRootEntityKey(rootEntityKind, slobId);
+    }
+
+    @Override public SlobId parseRootEntityKey(Key key) {
+      return MutationLog.parseRootEntityKey(rootEntityKind, key);
+    }
+  }
+
   public static void makeBasicBindingsAndExposures(PrivateBinder binder,
       Class<? extends Annotation> annotation) {
     binder.bind(SlobStore.class).to(SlobStoreImpl.class);
+    binder.bind(SlobFacilities.class).to(FacilitiesImpl.class);
     binder.install(factoryModule(MutationLogFactory.class, MutationLog.class));
 
     binder.bind(MutationLogFactory.class).annotatedWith(annotation).to(MutationLogFactory.class);
     binder.bind(SlobStore.class).annotatedWith(annotation).to(SlobStore.class);
     binder.bind(LocalMutationProcessor.class).annotatedWith(annotation)
         .to(LocalMutationProcessor.class);
+    binder.bind(SlobFacilities.class).annotatedWith(annotation).to(FacilitiesImpl.class);
+
     binder.expose(MutationLogFactory.class).annotatedWith(annotation);
     binder.expose(SlobStore.class).annotatedWith(annotation);
     binder.expose(LocalMutationProcessor.class).annotatedWith(annotation);
+    binder.expose(SlobFacilities.class).annotatedWith(annotation);
   }
 
   public static void bindEntityKinds(PrivateBinder binder, String prefix) {

commit 1f8f4a4154aafa65ea123349a95562b6dbb52875

wave/server/admin/StoreViewHandler: Fix store type.
This was presumably broken in revision cf9459a4.

src/com/google/walkaround/wave/server/admin/StoreViewHandler.java
@@ -61,7 +61,7 @@ public class StoreViewHandler extends AbstractHandler {
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-    String storeType = optionalParameter(req, "storeType", "CONV");
+    String storeType = optionalParameter(req, "storeType", "Wavelet");
     String id = optionalParameter(req, "id", "");
     String snapshotVersion = optionalParameter(req, "snapshotVersion", "");
     String historyStart = optionalParameter(req, "historyStart", "0");